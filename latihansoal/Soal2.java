9c88a1d42e306b10aa77198574f9213a3a8e943e
Rosintan Sitinjak <2381014@unai.edu>
2024-09-05 22:51:44 +0700

    Revisi dan penyelesaian Latsol - Handson 2

--- latihansoal/Soal2.java
+++ latihansoal/Soal2.java
@@ -1,5 +1,27 @@
 package latihansoal;
 
+import java.util.Scanner;
+
 public class Soal2 {
     // mencari nilai maksimum dari 3 bilangan
+
+    public static void main(String[] args) {
+        int[] bil = new int[3];
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < bil.length; i++) {
+            System.out.println("Masukan Bilangan ke - " + (i + 1) + " : ");
+            bil[i] = input.nextInt();
+        }
+
+        int largestNumber = bil[0];
+        for (int i = 1; i < bil.length; i++) {
+            if (largestNumber < bil[i]) {
+                largestNumber = bil[i];
+            }
+        }
+        System.out.println(largestNumber);
+
+//        input.close();
+    }
 }
--- latihansoal/Soal3.java
+++ latihansoal/Soal3.java
@@ -1,5 +1,58 @@
 package latihansoal;
 
+import java.util.Scanner;
+
 public class Soal3 {
     // kalkulator sederhana
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Aplikasi Kalkulator Sederhana");
+        System.out.println("Pilihlah operasi yang kamu inginkan: ");
+        System.out.println("1. Penjumlahan");
+        System.out.println("2. Pengurangan");
+        System.out.println("3. Perkalian");
+        System.out.println("4. Pembagian");
+
+        System.out.println("Masukan pilihan operasi (No. 1-4): ");
+        int select = input.nextInt();
+
+        System.out.println("Masukan bilangan pertama: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Masukan bilangan kedua: ");
+        int num2 = input.nextInt();
+
+        int result = 0;
+
+        switch (select) {
+            case 1:
+                result = num1 + num2;
+                System.out.println("Hasil dari penjumlahan = " + result);
+                break;
+
+            case 2:
+                result = num1 - num2;
+                System.out.println("Hasil dari pengurangan = " + result);
+                break;
+
+            case 3:
+                result = num1 * num2;
+                System.out.println("Hasil dari perkalian = " + result);
+                break;
+
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Hasil dari pembagian = " + result);
+                } else {
+                    System.out.println("Terjadi Kesalahan!! pembagian dengan nol tidak diperbolehkan");
+                }
+                break;
+            default:
+                System.out.println("Pilihan tidak ada!");
+        }
+        input.close();
+    }
 }
